296f307c2efeeffb0e2995374c09264625dd870d
abhu001987 <Abhikr18996@gmail.com>
2026-03-12 16:25:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ fetch("https://api.github.com/repos/abhu001987/Hotterbite/contents/posts")
 
 let slider = document.getElementById("slider")
 
-files.forEach(file => {
+files.reverse().forEach(file => {
 
 fetch(file.download_url)
 
@@ -24,21 +24,35 @@ let title = data.match(/title:\s*(.*)/)[1]
 let cat = data.match(/category:\s*(.*)/)[1]
 let image = data.match(/image:\s*(.*)/)[1]
 
+let date = file.name.split("-").slice(0,3).join("-")
+
 if(category === "all" || cat === category){
 
 slider.innerHTML += `
 
 <div class="post">
 
+<div style="position:relative">
+
 <img src="${image}">
 
 <a href="https://en.wikipedia.org/wiki/India" class="back-btn">←</a>
+
+</div>
 
 <div class="content">
 
 <div class="title">${title}</div>
 
 <div class="text">${content}</div>
+
+</div>
+
+<div class="post-footer">
+
+<div class="category-badge">${cat}</div>
+
+<div class="post-date">${date}</div>
 
 </div>
 
